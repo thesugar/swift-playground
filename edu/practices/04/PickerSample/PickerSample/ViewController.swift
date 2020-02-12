dae0447ca4d92229bdcf9d96b74ce4b701a461cb
User <user@Student.local>
2020-02-12 17:00:51 +0900

    1st day end

--- edu/practices/04/PickerSample/PickerSample/ViewController.swift
+++ edu/practices/04/PickerSample/PickerSample/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     /// 画像の表示名とファイル名を持った、タプルの配列
     let imageNames: [(title: String, fileName: String)] = [
@@ -10,12 +10,35 @@ class ViewController: UIViewController {
         ("フクロウ", "owl"),
         ("クラゲ", "jelly-fish")
     ]
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerView.dataSource = self
+        pickerView.delegate = self
     }
+    
+    // MARK: - Picker View DataSource
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageNames.count
+    }
+    
+    // MARK: - Picker View Delegate
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return imageNames[row].title
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        imageView.image = UIImage(named: imageNames[row].fileName)
+    }
 }
 
